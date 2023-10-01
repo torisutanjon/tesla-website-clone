5ae75ebde64c3a63157d105984dc7cedd3db5b00
Tristan John P. Girao <giraotristan@gmail.com>
2023-10-01 15:43:27 +0800

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
   return (
     <div className="relative h-40 w-full bg-black snap-end flex flex-col items-center justify-evenly py-4 text-white/75 text-sm font-medium lg:hidden">
       <div className="absolute -mt-12 h-8 w-8" ref={observeRef}></div>
-      <p>Tesla &copy; 2023</p>
+      <p>Trsitan John P. Girao &copy; 2023</p>
       <p>Privacy & Legal</p>
       <p>Vehicle Recalls</p>
       <p>News</p>
